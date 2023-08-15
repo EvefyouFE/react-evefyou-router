06aa7abdc4d5f5f943a66e6aa8cd07862bf78404
evef <eveforest@163.com>
2023-08-15 22:21:48 +0800

    export CrumbData

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,2 +1,10 @@
+/*
+ * @Author: EvefyouFE
+ * @Date: 2023-08-15 01:26:31
+ * @FilePath: \react-evefyou-router\src\routes\index.ts
+ * @Description: 
+ * Everyone is coming to the world i live in, as i am going to the world lives for you. 人人皆往我世界，我为世界中人人。
+ * Copyright (c) 2023 by EvefyouFE/evef, All Rights Reserved. 
+ */
 export * from './crRoutes';
 export type { CrumbData } from './props'

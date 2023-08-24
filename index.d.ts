a5ca28a101720246fd249360b20f630d6cba16de
evef <eveforest@163.com>
2023-08-24 17:02:08 +0800

    change rollup config

---
+++ index.d.ts
@@ -0,0 +1,85 @@
+import { LazyRouteFunction } from 'react-router';
+import { RouteObject } from 'react-router';
+
+export declare type CrRouteObject = Omit<RouteObject, 'children' | 'lazy'> & Omit<RouteMenuItem, 'path'> & {
+    children?: CrRouteObject[] | undefined;
+    lazy?: LazyRouteFunction<any>;
+};
+
+export declare interface CrRouteOptions {
+    path?: string;
+    wrapComponent?: WrapComponent;
+    errorElement?: React.ReactNode;
+    loader?: () => Promise<any>;
+    handleFn?: GlobHandleFn;
+    isIndex?: boolean;
+}
+
+export declare type CrRouteViewConfig = {
+    [key: string]: CrRouteOptions;
+};
+
+export declare interface CrumbData {
+    title: string;
+    menuTreeList?: MenuTreeList;
+}
+
+export declare function generateCrRoutes(modules: Record<string, () => Promise<PageModule>>, config: CrRouteViewConfig): CrRouteObject[];
+
+export declare function generateCrViewsPaths(viewModules: Record<string, () => Promise<PageModule>>): string[];
+
+export declare type GlobHandleFn = (opt?: CrRouteObject) => any;
+
+export declare type LazyModule = Promise<PageModule>;
+
+export declare type LazyModuleFn = () => LazyModule;
+
+export declare interface MenuItem {
+    /** menu item name */
+    name: string;
+    /** menu labels */
+    label?: {
+        zh_CN: string;
+        en_US: string;
+    };
+    /** 图标名称
+     *
+     * 子子菜单不需要图标
+     */
+    icon?: string;
+    /** 菜单id */
+    key: string;
+    /** 菜单路由 */
+    path: string;
+    /** 子菜单 */
+    children?: MenuItem[];
+    /**
+     * 国际化配置
+     */
+    locale?: string;
+}
+
+export declare type MenuTreeList = MenuItem[];
+
+export declare type ModulesObject = Record<string, LazyModuleFn>;
+
+export declare type PageModule = {
+    default: React.ComponentType<Recordable | object>;
+    [key: string]: React.ComponentType<Recordable | object>;
+};
+
+export declare type Recordable<T = any> = Record<string, T>;
+
+export declare interface RouteMenuItem {
+    name?: string;
+    path: string;
+    locale?: string;
+}
+
+export declare type RouteModulesObject = Record<string, LazyModuleFn | ModulesObject>;
+
+export declare type RoutePathConfig = Record<string, RouteModulesObject>;
+
+export declare type WrapComponent = (component: React.ComponentType<any>) => React.ComponentType<any> | null;
+
+export { }
--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,50 @@
 /*
  * @Author: EvefyouFE
  * @Date: 2023-08-10 13:42:48
- * @FilePath: \react-evefyou-hooks\vite.config.ts
+ * @FilePath: \react-evefyou-router\vite.config.ts
  * @Description: 
  * Everyone is coming to the world i live in, as i am going to the world lives for you. 人人皆往我世界，我为世界中人人。
  * Copyright (c) 2023 by EvefyouFE/evef, All Rights Reserved. 
  */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import typescript from "@rollup/plugin-typescript";
 import path from "path";
 import dts from 'vite-plugin-dts';
+import pkg from './package.json';
+
+const pathResolve = (v: string) => path.resolve(__dirname, v)
+
+const externalPackages = [...Object.keys(pkg.peerDependencies)]
+const regexOfPackages = externalPackages
+  .map(packageName => new RegExp(`^${packageName}(\\/.*)?`));
 
 export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: "~",
-        replacement: path.resolve(__dirname, "./src"),
-      },
-    ],
-  },
-  plugins: [react(), dts({ insertTypesEntry: true })],
+  plugins: [react(), dts({ rollupTypes: true })],
   build: {
-    manifest: true,
     minify: true,
     reportCompressedSize: true,
+    outDir: '.',
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      fileName: "index",
+      entry: {
+        "index": pathResolve('src/index.ts')
+      },
+      fileName(format) {
+        return `${format}/index.js`
+      },
+      name: "react-evefyou-router",
       formats: ["es", "cjs"],
     },
-    sourcemap: true,
     rollupOptions: {
-      plugins: [typescript({
-        sourceMap: true,
-        declaration: true,
-        outDir: 'dist'
-      })],
-      external: ['react', 'react-dom', 'ramda', 'react-router', 'react-router-dom']
+      output: {
+        manualChunks: () => {
+          return 'index'
+        },
+        chunkFileNames: () => {
+          return '[format]/[name]/index.js'
+        },
+        assetFileNames: '[ext]/[name].[ext]',
+      },
+      external: regexOfPackages
     }
   }
 })
